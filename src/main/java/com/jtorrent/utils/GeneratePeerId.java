7a9f5726ca4bee13ca40c39acdcc287389feae66
RudraPrakash003 <rudraprakash999@gmail.com>
2025-12-17 21:41:29 +0530

    Added Peer ID generator

---
+++ src/main/java/com/jtorrent/utils/GeneratePeerId.java
@@ -0,0 +1,17 @@
+package com.jtorrent.utils;
+
+import java.security.SecureRandom;
+
+public class GeneratePeerId {
+    private static byte[] peerId;
+    public static byte[] generateId() {
+        if(peerId == null) {
+            peerId = new byte[20];
+            new SecureRandom().nextBytes(peerId);
+
+            byte[] prefix = "-JT00001-".getBytes();
+            System.arraycopy(prefix, 0, peerId, 0, prefix.length);
+        }
+        return peerId;
+    }
+}

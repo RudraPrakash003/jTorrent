111867afd439dcc01a884a4585385c3cdee3110a
RudraPrakash003 <rudraprakash999@gmail.com>
2025-12-17 20:53:25 +0530

    Added InfoHash

---
+++ src/main/java/com/jtorrent/metaInfo/InfoHash.java
@@ -0,0 +1,28 @@
+package com.jtorrent.metaInfo;
+
+import com.jtorrent.bencoder.BEncoder;
+
+import java.security.MessageDigest;
+import java.util.Map;
+
+public class InfoHash {
+
+    public static byte[] getInfoHash(Map<String, Object> torrent) {
+        Object info = torrent.get("info");
+        if(!(info instanceof Map)) {
+            throw new IllegalArgumentException("Info dictionary missing");
+        }
+
+        Map<String, Object> infoMap = (Map<String, Object>) info;
+
+        byte[] encodedInfo = BEncoder.encode(infoMap);
+
+        try {
+            MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
+            return sha1.digest(encodedInfo);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}

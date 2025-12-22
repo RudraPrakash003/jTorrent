0fce15ed83a975cd8c3009df838c5b98a3fa198a
RudraPrakash003 <rudraprakash999@gmail.com>
2025-12-22 11:16:39 +0530

    Made changes to TorrentClient

--- src/main/java/com/jtorrent/TorrentClient.java
+++ src/main/java/com/jtorrent/TorrentClient.java
@@ -1,51 +1,68 @@
 package com.jtorrent;
 
 import com.jtorrent.bencoder.BDecoder;
-import com.jtorrent.tracker.Tracker;
 import com.jtorrent.metaInfo.InfoHash;
+import com.jtorrent.peer.PeerManager;
+import com.jtorrent.tracker.Tracker;
 
-import java.io.InputStream;
+import java.io.File;
+import java.io.FileInputStream;
+//import java.io.InputStream;
 import java.util.Map;
 
 public class TorrentClient {
     public static void main(String[] args) {
-        String resource = "Tron Ares.torrent";
-        ClassLoader cl = TorrentClient.class.getClassLoader();
+        if(args == null) {
+            System.err.println("Torrent file is not provided.");
+            System.exit(1);
+        }
 
-        try(InputStream input = cl.getResourceAsStream(resource)) {
-            if(input == null) throw new RuntimeException("Resource not found: " + resource);
+        File resource = new File(args[0]);
 
-            byte[] data = input.readAllBytes();
+        if(!resource.exists() || !resource.isFile()) {
+            throw new IllegalArgumentException("Torrent file does not exist or is not a file.");
+        }
+
+//        String resource = "Tron Ares.torrent";
+//        ClassLoader cl = TorrentClient.class.getClassLoader();
+
+//        try(InputStream input = cl.getResourceAsStream(resource)) {
+        try(FileInputStream input = new FileInputStream(resource)) {
+//           if(input == null) throw new RuntimeException("Resource not found: " + resource);
+
+           byte[] data = input.readAllBytes();
 //              String text = new String(data, StandardCharsets.ISO_8859_1);
 //              System.out.println(text);
 
-            BDecoder decoder = new BDecoder(data);
-            Object decoded =  decoder.decode();
-            if(!(decoded instanceof Map)) {
-                throw new RuntimeException("Decode error");
-            }
-            Map<String, Object> torrent = (Map<String, Object>) decoded;
+           BDecoder decoder = new BDecoder(data);
+           Object decoded =  decoder.decode();
+           if(!(decoded instanceof Map)) {
+               throw new RuntimeException("Decode error");
+           }
+           Map<String, Object> torrent = (Map<String, Object>) decoded;
 
-//              torrent.forEach((k,v)->{ System.out.println(k + ":" + v);});
+           //              torrent.forEach((k,v)->{ System.out.println(k + ":" + v);});
+
+
+           byte[] infoHash = InfoHash.getInfoHash(torrent);
+           if(infoHash == null || infoHash.length != 20) {
+               throw new RuntimeException("Invalid InfoHash length");
+           }
+//            System.out.println("Info Hash: " + ByteConvertor.bytesToHex(infoHash));
+
             Object announceObj = torrent.get("announce");
             if(announceObj == null) {
                 throw new RuntimeException("Announce key not found in torrent file");
             }
             String announce = announceObj.toString();
-//            System.out.println("Announce: " + announce);
-
-            byte[] infoHash = InfoHash.getInfoHash(torrent);
-            if(infoHash == null || infoHash.length != 20) {
-                throw new RuntimeException("Invalid InfoHash length");
-            }
-//            System.out.println("Info Hash: " + ByteConvertor.bytesToHex(infoHash));
-
+//           System.out.println("Announce: " + announce);
 
             Tracker tracker = new Tracker();
             tracker.getPeers(announce, infoHash, torrent, peers -> {
-                System.out.println("Peers:");
-                peers.forEach(System.out::println);
+                PeerManager peerManager = new PeerManager(infoHash);
+                peerManager.connectToPeers(peers);
             });
+
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
